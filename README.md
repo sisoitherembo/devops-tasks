23bed645470550612a6cf5816fa8468df150a831
DmitryZarubo <dmitri2002zarubo@mail.ru>
2022-08-15 19:11:38 +0300

    refactoring shell scripts and readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Tutorial:
 - To start an application without docker compose - execute "$ ./start_docker.sh" script 
+- To start an application without docker compose and to push built images to DockerHub - execute "$ ./start_docker.sh push_images" 
 - To stop an application without docker compose - execute "$ ./stop_docker.sh" script
 - To start an appication with docker compowe - execute "$ docker compose up" in shell
 
--- start_docker.sh
+++ start_docker.sh
@@ -12,12 +12,9 @@ apache_conf_dir="apache-config"
 apache_dockerfile="apache.dockerfile"
 apache_port_mapping="8080:80"
 
-ids=$(docker ps -a |  awk  '{ if ( $13 == "nginx-app" || $12 == "apache-app") print $1;}')
-if [ -n "$ids" ]; then
-    echo "Deleting existing containers..."
-    docker rm -f $ids
-fi
+echo "Starting our app..."
 
+echo "Building our images..."
 if [ -d $apache_conf_dir ]; then
     echo "Found apache-conf dir" && cd $apache_conf_dir 
     if [ -f $apache_dockerfile ]; then
@@ -25,6 +22,9 @@ if [ -d $apache_conf_dir ]; then
         docker build -f $apache_dockerfile -t $dockerhub_login/$apache_name:latest . && cd ..
         if [ $? == 1 ]; then 
             echo "Something went wrong when building apache.." && exit 1 
+        elif [ "$1" == "push_images" ]; then
+            echo "Pushing apache image to dockerhub"
+            docker push $dockerhub_login/$apache_name:latest
         fi
     fi
 fi
@@ -35,12 +35,23 @@ if [ -d $nginx_conf_dir ]; then
         docker build -f $nginx_dockerfile -t $dockerhub_login/$nginx_name:latest . && cd ..
         if [ $? == 1 ]; then 
             echo "Something went wrong when building apache.." && exit 1 
+        elif [ "$1" == "push_images" ]; then
+            echo "Pushing nginx image to dockerhub"
+            docker push $dockerhub_login/$nginx_name:latest
         fi
     fi
 fi
+
+echo "Checking for name conflicts..."
+ids=$(docker ps -a |  awk -vAPP1=$nginx_name -vAPP2=$apache_name '{ if ( $13 == APP1 || $12 == APP2 ) print $1;}')
+if [ -n "$ids" ]; then
+    echo "Found name conflicts! Deleting existing containers..."
+    docker rm -f $ids
+fi
+
 echo "Starting nginx-apache application..."
-docker run -d -p $apache_port_mapping --name $apache_name --network $network $apache_name:latest \
-    && docker run -p $nginx_port_mapping  --network $network --name $nginx_name -v $host_mountpoint:$container_mountpoint -d  $nginx_name:latest 
+docker run -d -p $apache_port_mapping --name $apache_name --network $network $dockerhub_login/$apache_name:latest \
+    && docker run -p $nginx_port_mapping  --network $network --name $nginx_name -v $host_mountpoint:$container_mountpoint -d  $dockerhub_login/$nginx_name:latest 
 if [ $? == 1 ]; then 
     echo "Something went wrong when building apache.." && exit 1 
 fi

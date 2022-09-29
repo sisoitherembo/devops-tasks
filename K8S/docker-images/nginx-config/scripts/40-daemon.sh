3dc44cb020d990e1247c4ece4ae6619b6245e159
sisoitherembo <dmitri2002zarubo@mail.ru>
2022-09-29 10:57:34 +0300

    added k8s nginx-app deploy

--- K8S/docker-images/nginx-config/scripts/40-daemon.sh
+++ K8S/docker-images/nginx-config/scripts/40-daemon.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 /home/nginx-log-daemon.sh  & 
+sudo snap install -y --classic  certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo certbot -n -d sisoi-k8s.ddns.net -m dmitri20023zarubo@gmail.com --nginx
